9ab1eb174c7fa19c73baf1ce2735e7049492f9f8
Dani <daniel@tootsweet.app>
2019-02-19 19:07:13 +0100

    Define the cache limit to 50 mb

--- android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
@@ -1,9 +1,19 @@
 package com.dylanvann.fastimage;
 
+import android.content.Context;
+
+import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
 import com.bumptech.glide.module.AppGlideModule;
 
 // We need an AppGlideModule to be present for progress events to work.
 @GlideModule
 public final class FastImageGlideModule extends AppGlideModule {
+
+    @Override
+    public void applyOptions(Context context, GlideBuilder builder) {
+        int diskCacheSizeBytes = 1024 * 1024 * 50;
+        builder.setDiskCache(new InternalCacheDiskCacheFactory(context, "ts-glide", diskCacheSizeBytes));
+    }
 }
